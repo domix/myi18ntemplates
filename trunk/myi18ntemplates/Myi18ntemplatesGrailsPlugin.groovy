a6ef302e0c9b80368fe4fae56ff8096416bdb90a
domingo.suarez <domingo.suarez@a9825aa9-a447-0410-a6af-25c1305c5d8c>
2008-03-04 23:58:22 +0000

    added plugin properties and deleted not used code

--- trunk/myi18ntemplates/Myi18ntemplatesGrailsPlugin.groovy
+++ trunk/myi18ntemplates/Myi18ntemplatesGrailsPlugin.groovy
@@ -2,30 +2,9 @@
 class Myi18ntemplatesGrailsPlugin {
     def version = 0.1
     def dependsOn = [:]
-	
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-   
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)		
-    }
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional)
-    }
-	                                      
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-	
-    def onChange = { event ->
-        // TODO Implement code that is executed when this class plugin class is changed  
-        // the event contains: event.application and event.applicationContext objects
-    }
-                                                                                  
-    def onApplicationChange = { event ->
-        // TODO Implement code that is executed when any class in a GrailsApplication changes
-        // the event contain: event.source, event.application and event.applicationContext objects
-    }
+	def author = "Domingo Suarez Torres"
+	def authorEmail = "domingo.suarez@gmail.com"
+	def title = "My I18n Templates"
+	def description = "This plugin offers i18n scaffolding templates to generate a full internationalizable application."
+	def documentation = "http://code.google.com/p/myi18ntemplates/wiki/Plugin"
 }
