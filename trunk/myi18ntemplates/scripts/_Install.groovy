4010dab177d4178776927ee21dfe9dd3e4379af5
domingo.suarez <domingo.suarez@a9825aa9-a447-0410-a6af-25c1305c5d8c>
2008-03-05 00:02:35 +0000

    Steal code from Marcel Overdijk

--- trunk/myi18ntemplates/scripts/_Install.groovy
+++ trunk/myi18ntemplates/scripts/_Install.groovy
@@ -1,14 +1,30 @@
+/*
+ * Copyright 2004-2005 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-//
-// This script is executed by Grails after plugin was installed to project.
-// This script is a Gant script so you can use all special variables provided
-// by Gant (such as 'baseDir' which points on project base dir). You can
-// use 'Ant' to access a global instance of AntBuilder
-//
-// For example you can create directory under project tree:
-// Ant.mkdir(dir:"/Users/domix/proyectos/myi18ntemplates/myi18ntemplates/grails-app/jobs")
-//
+/**
+ * Installs the i18n scaffolding templates
+ *
+ * @author Marcel Overdijk
+ */
 
-Ant.property(environment:"env")
-grailsHome = Ant.antProject.properties."env.GRAILS_HOME"
+Ant.copy(todir: "${basedir}/src/templates/scaffolding", overwrite: true) {
+    fileset(dir: "${pluginBasedir}/src/grails/templates/scaffolding")
+}
 
+// Workaround for GRAILS-1668: Resource bundles contained by plugins are not resolved by the I18nGrailsPlugin
+Ant.copy(todir: "${basedir}/grails-app/i18n", overwrite: true) {
+    fileset(dir: "${pluginBasedir}/grails-app/i18n")
+}
