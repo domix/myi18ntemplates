e211e9dae9cfc1cfece80d5999f45d53e204014c
domingo.suarez <domingo.suarez@a9825aa9-a447-0410-a6af-25c1305c5d8c>
2008-03-05 16:31:01 +0000

    0.1 plugin release

---
+++ tags/myi18ntemplates-0.1/Myi18ntemplatesGrailsPlugin.groovy
@@ -0,0 +1,10 @@
+
+class Myi18ntemplatesGrailsPlugin {
+    def version = 0.1
+    def dependsOn = [:]
+	def author = "Domingo Suarez Torres"
+	def authorEmail = "domingo.suarez@gmail.com"
+	def title = "My I18n Templates"
+	def description = "This plugin offers i18n scaffolding templates to generate a full internationalizable application."
+	def documentation = "http://code.google.com/p/myi18ntemplates/wiki/Plugin"
+}
